8865162782fe0a544e6a44476d31a143578a17ca
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-01-16 21:46:03 +0530

    build user model

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 40,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: { type: String, required: true },
+  profileName: { type: String },
+  bio: { type: String, maxlength: 150 },
+  accountStatus: {
+    type: String,
+    enum: ["active", "disable", "banned"],
+    default: "active",
+  },
+  isVerified: { type: Boolean, default: false },
+  isPrivate: { type: Boolean, default: false },
+  gender: {
+    type: String,
+    enum: ["male", "female", "non-binary", "prefer not to say"],
+  },
+  phoneNumber: { type: String, trim: true },
+});
+
+export default mongoose.model("User", userSchema);
